7d5c071a8a74afb4ccd7ebd8dae3b52e7696f63b
Erik Butcher <butcer0@gmail.com>
2020-06-13 15:55:21 -0400

    Introduced Lodash Memoized Thunk Redux Action
    Introduced Lodash Memoized Thunk Redux Action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 export const fetchPosts = () => async dispatch => {
@@ -6,12 +7,20 @@ export const fetchPosts = () => async dispatch => {
     dispatch({type: 'FETCH_POSTS', payload: response.data});
 };
 
-export const fetchUser = id => async dispatch => {
+// syntax cleaned up below
+// export const fetchUser = id => dispatch => {
+//    _fetchUser(id, dispatch);
+// };
+
+// lodash memoization automatically caches response to prevent
+//  unnecessary API calls
+// note: this will only allow you to retrieve the user only once
+export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+const _fetchUser = _.memoize(async (id, dispatch) => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
 
     dispatch({type: 'FETCH_USER', payload: response.data});
-};
-
+});
 
 // refactored to thunk style arrow syntax simplified
 // export const fetchPosts =  () => {
