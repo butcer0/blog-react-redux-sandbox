b2ac2141354e7d4dea79660c40eeee9b11f7dab0
Erik Butcher <butcer0@gmail.com>
2020-06-13 16:22:31 -0400

    Async Actions Calling Actions and Lodash Chaining
    Async Actions Calling Actions and Lodash Chaining

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,26 +1,46 @@
 import _ from 'lodash';
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value()
+
+    // replaced by chain notation above
+    // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    // userIds.forEach(id => dispatch(fetchUser(id)));
+};
+
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
 
     dispatch({type: 'FETCH_POSTS', payload: response.data});
 };
 
-// syntax cleaned up below
-// export const fetchUser = id => dispatch => {
-//    _fetchUser(id, dispatch);
-// };
+export const fetchUser = id => async dispatch => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({type: 'FETCH_USER', payload: response.data});
+};
 
 // lodash memoization automatically caches response to prevent
 //  unnecessary API calls
 // note: this will only allow you to retrieve the user only once
-export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
-const _fetchUser = _.memoize(async (id, dispatch) => {
-    const response = await jsonPlaceholder.get(`/users/${id}`);
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+//
+//     dispatch({type: 'FETCH_USER', payload: response.data});
+// });
 
-    dispatch({type: 'FETCH_USER', payload: response.data});
-});
+// syntax cleaned up above
+// export const fetchUser = id => dispatch => {
+//    _fetchUser(id, dispatch);
+// };
 
 // refactored to thunk style arrow syntax simplified
 // export const fetchPosts =  () => {
